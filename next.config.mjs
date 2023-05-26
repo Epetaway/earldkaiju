81587c0c014836ee9c2266b707540c0046dfa965
Earl <earlpetaway@gmail.com>
2023-05-26 08:51:29 -0400

    this should be working

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,24 @@ import remarkCodeTitles from './src/lib/remark-code-title.mjs';
 import rehypePresetMinify from 'rehype-preset-minify';
 import nextMDX from '@next/mdx';
 
+const isProd = process.env.NODE_ENV === 'production';
+
+// Replace 'your-repo-name' with the name of your repository
+const repo = 'Tailwind-Site';
+
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
+  // Use the repo as the assetPrefix and basePath
+  assetPrefix: isProd ? `/${repo}/` : '',
+  basePath: isProd ? `/${repo}` : '',
 };
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkGfm, remarkCodeTitles],
-    rehypePlugins: [rehypePrismPlus, rehypePresetMinify],
+    rehypePlugins: [rehypePrismPlus, rehypePreset-minify],
   },
 });
 
