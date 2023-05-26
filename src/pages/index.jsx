0ba3baeccd39547f5ced54315735d10549b7f9eb
Earl <earlpetaway@gmail.com>
2023-05-26 14:58:02 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import { getImageLoader } from 'next/image';
 import Link from 'next/link';
 import clsx from 'clsx';
-
+import Image from 'next/image'
 import { Button } from '@/components/Button';
 import { Card } from '@/components/Card';
 import { Container } from '@/components/Container';
